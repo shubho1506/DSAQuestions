a7acb5841c0488c156ca6d55e43e9dee53be17cb
shubho1506 <sds15shubho@gmail.com>
2025-06-26 19:17:00 +0530

    DSA

---
+++ src/StackAndQueues/ImplementQueueUsingStacks.java
@@ -0,0 +1,51 @@
+package StackAndQueues;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStacks {
+    Stack<Integer> s1;
+
+    public ImplementQueueUsingStacks(){
+        s1 = new Stack<>();
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        Stack<Integer> temp = new Stack<>();
+        while(!s1.isEmpty()){
+            temp.push(s1.pop());
+        }
+        int a = temp.pop();
+        while(!temp.isEmpty()){
+            s1.push(temp.pop());
+        }
+        return a;
+    }
+
+    public int peek() {
+        Stack<Integer> temp = new Stack<>();
+        while(!s1.isEmpty()){
+            temp.push(s1.pop());
+        }
+        int a = temp.peek();
+        while(!temp.isEmpty()){
+            s1.push(temp.pop());
+        }
+        return a;
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ImplementQueueUsingStacks queue = new ImplementQueueUsingStacks();
+        queue.push(20);
+        queue.push(10);
+        queue.push(40);
+        System.out.println(queue.pop());
+    }
+}
---
+++ src/StackAndQueues/ImplementStackUsingQueues.java
@@ -0,0 +1,66 @@
+package StackAndQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueues {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public ImplementStackUsingQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        if (!q1.isEmpty()) {
+            q1.offer(x);
+        }
+        else q2.offer(x);
+    }
+
+    public int pop() {
+        int x = 0;
+        if (!q1.isEmpty()) {
+            while (!q1.isEmpty()) {
+                x = q1.poll();
+                if (!q1.isEmpty()) q2.offer(x);
+            }
+        } else {
+            while (!q2.isEmpty()) {
+                x = q2.poll();
+                if (!q2.isEmpty()) q1.offer(x);
+            }
+        }
+        return x;
+    }
+
+    public int top() {
+        int x = 0;
+        if (!q1.isEmpty()) {
+            while (!q1.isEmpty()) {
+                x = q1.poll();
+                q2.offer(x);
+            }
+        } else {
+            while (!q2.isEmpty()) {
+                x = q2.poll();
+                q1.offer(x);
+            }
+        }
+        return x;
+    }
+
+    public boolean empty() {
+        return q1.isEmpty() && q2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ImplementStackUsingQueues stack = new ImplementStackUsingQueues();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+    }
+}
